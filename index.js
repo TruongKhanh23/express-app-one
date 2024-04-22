09b93e5a69a10156ce27184e4091c0b4c00c4114
tn.khanh <truongn.khanh@dai-ichi-life.com.vn>
2024-04-22 17:59:47 +0700

    [TNK-015] Route Middleware

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ var express = require("express");
 
 app = express();
 
-app.use(function(request, response, next){
-  console.log("I am from Middleware validation");
-  next()
-})
-
 app.get("/", function(request, response){
   response.send("This is home page")
+})
+
+app.use("/about", function(request, response, next){
+  console.log("I am from About validation");
+  next()
 })
 
 app.get("/about", function(request, response){
